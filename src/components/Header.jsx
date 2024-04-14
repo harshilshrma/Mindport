017257feb89b863bfddea8903021181b3eb856a7
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-04-14 23:30:01 +0530

    completed initial setting

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Mindport</h1>
+            <h5>A portal for your thoughts.</h5>
+        </header>
+    )
+}
+
+export default Header;
