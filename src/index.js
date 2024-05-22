99a02f1519440c93ac00567d290c65805c98b934
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-05-22 17:49:54 +0530

    vsv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App />);
-
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
